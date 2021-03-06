3ec03d003e5c72f47cbd96224f60df29f5ab322c
Lucas Nardon <lucaslnardon@gmail.com>
2021-03-06 02:34:25 -0300

    ✨ Add theme switcher

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React from "react";
 import "./app.css";
 import play from "./play.svg";
 
+const themes = [
+  {
+    title: "CLASSIC BLACK",
+    appBg: "#131313",
+    timeDisplay: "#e9e9e9",
+    closeBtn: "rgb(255, 92, 92)",
+    minimizeBtn: "rgb(255, 223, 81)",
+    menuLabels: "#e9e9e9",
+    menuSelectBorder: "#e9e9e9",
+  },
+  {
+    title: "CLASSIC WHITE",
+    appBg: "#e9e9e9",
+    timeDisplay: "#131313",
+    closeBtn: "rgb(255, 92, 92)",
+    minimizeBtn: "rgb(255, 223, 81)",
+    menuLabels: "#131313",
+    menuSelectBorder: "#131313",
+  },
+];
 const App = () => {
   const startTimer = (time) => {
     let timerId;
@@ -12,7 +32,10 @@ const App = () => {
         globalTimer--;
       } else {
         clearInterval(timerId);
-        fireNotification("YEEEEE HAAAAAAAAA", "Time is up");
+        fireNotification(
+          "End of period.",
+          "Take a short break and prepare for the next one."
+        );
       }
     }, 1000);
   };
@@ -48,11 +71,25 @@ const App = () => {
     }
   };
 
+  const themeSwitcher = (e) => {
+    const selected = e.target.value;
+    const root = document.documentElement;
+    root.style.setProperty("--appBg", themes[selected]["appBg"]);
+    root.style.setProperty("--timeDisplay", themes[selected]["timeDisplay"]);
+    root.style.setProperty("--closeBtn", themes[selected]["closeBtn"]);
+    root.style.setProperty("--minimizeBtn", themes[selected]["minimizeBtn"]);
+    root.style.setProperty("--menuLabels", themes[selected]["menuLabels"]);
+    root.style.setProperty(
+      "--menuSelectBorder",
+      themes[selected]["menuSelectBorder"]
+    );
+  };
+
   return (
     <div class="container">
       <div class="systemBtns">
         <div onClick={closeApp} class="btn close"></div>
-        <div onClick={hideApp} class="btn minimize"></div>
+        <div onClick={toggleMenu} class="btn minimize"></div>
       </div>
 
       {/* HOME */}
@@ -69,13 +106,15 @@ const App = () => {
       {/* MENU */}
       <div class="menu">
         <div class="configDiv">
-          <label for="theme">Theme:</label>
-          <select name="theme" id="theme">
-            <option select value="0">
-              White
+          <label className="label" for="theme">
+            Theme:
+          </label>
+          <select name="theme" id="theme" onChange={(e) => themeSwitcher(e)}>
+            <option selected value="0">
+              Black
             </option>
-            <option value="1">Black</option>
-            <option value="2">Neon</option>
+            <option value="1">White</option>
+            {/* <option value="2">Neon</option> */}
           </select>
         </div>
       </div>
